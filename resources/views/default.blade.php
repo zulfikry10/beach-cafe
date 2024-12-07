905e9ef4db5535ddedfbc1f7c6d527c8e6d0034d
apikammar <zulfikryzuki2002@gmail.com>
2024-12-07 13:30:36 +0800

    default layout, feedback relay

---
+++ resources/views/default.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    @foreach (range(1,5) as $i)
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi repudiandae accusamus explicabo sunt neque, soluta non rerum ratione enim id dolorum molestias pariatur, minima amet cupiditate minus velit aperiam.
+    @endforeach
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="{{ asset('asset/default-image/beach-cafe.png') }}" type="image/png">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poller+One&display=swap" rel="stylesheet">
+
+    <title>Beach Cafe</title>
+
+    @vite(['resources/css/app.css'])
+</head>
+<body style="background-color: rgba(243, 245, 250, 1)">
+    <div>
+        @include('layouts.navigation')
+        <div class="fade-in">
+            <div class="d-flex justify-content-between align-items-center main-background">
+                <div class="h1 fw-bold ms-5" style="font-family: 'Poller One', sans-serif;">BEACH CAFE</div>
+                <img class="main-img me-5" src="{{ asset('asset/default-image/beach-cafe.png') }}" alt="beach-cafe.png">
+            </div>
+            <div class="mt-4 ms-4 me-4">
+                @yield('content')
+            </div>
+        </div>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    </div>
+</body>
+</html>
